df854592f04b6d1b9fa3336010745ba1fc02e016
Adam <budzinski.adam@gmail.com>
2020-01-13 13:52:27 +0100

    change app text

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          nigdy nie bedziesz jak Gilu
+          feature branch test
         </p>
       </div>
     );
